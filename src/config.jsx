370d83bd4522932f86b6053acceba99351cade14
Javier Delgado <javiercambranis23@gmail.com>
2023-02-14 09:01:07 -0500

    .

--- src/config.jsx
+++ src/config.jsx
@@ -1,8 +1,7 @@
 import { io } from 'socket.io-client'
 
 const ws = io("https://0d44-187-157-104-191.ngrok.io", {
-    withCredentials: true
-}
-);
+    transports: ['websockets', 'pollig', 'flashsocket']
+});
 
 export { ws }
